2fcf86c955b02918ce14589bb63110b9d2bdddf7
bianjian <biansant@qq.com>
2016-08-05 13:01:03 +0800

    .

---
+++ src/Server/test/mesagebus/MessageBus.hpp
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include <string>
+#include <functional>
+#include <map>
+#include "Any.hpp"
+#include "function_traits.hpp"
+#include "NonCopyable.hpp"
+
+class MessageBus: NonCopyable
+{
+public:
+    // 注册主题
+	template<typename F>
+	void Attach(F&& f, const std::string& strTopic="")
+	{
+		auto func = to_function(std::forward<F>(f));
+		Add(strTopic, std::move(func));
+	}
+    
+    // 发送消息
+	template<typename R>
+	void SendReq(const std::string& strTopic = "")
+	{
+		using function_type = std::function<R()>;
+		std::string strMsgType =strTopic+ typeid(function_type).name();
+		auto range = m_map.equal_range(strMsgType);
+		for (Iterater it = range.first; it != range.second; ++it)
+		{
+			auto f = it->second.AnyCast < function_type >();
+			f();
+		}
+	}
+	template<typename R, typename... Args>
+	void SendReq(Args&&... args, const std::string& strTopic = "")
+	{
+		using function_type = std::function<R(Args...)>;
+		std::string strMsgType =strTopic+ typeid(function_type).name();
+		auto range = m_map.equal_range(strMsgType);
+		for (Iterater it = range.first; it != range.second; ++it)
+		{
+			auto f = it->second.AnyCast < function_type >();
+			f(std::forward<Args>(args)...);
+		}
+	}
+
+	//移除某个主题, 需要主题和消息类型
+	template<typename R, typename... Args>
+	void Remove(const std::string& strTopic = "")
+	{
+		using function_type = std::function<R(Args...)>; //typename function_traits<void(CArgs)>::stl_function_type;
+
+		string strMsgType =strTopic +typeid(function_type).name();
+		int count = m_map.count(strMsgType);
+		auto range = m_map.equal_range(strMsgType);
+		m_map.erase(range.first, range.second);
+	}
+
+private:
+	template<typename F>
+	void Add(const std::string& strTopic, F&& f)
+	{
+		std::string strMsgType = strTopic + typeid(F).name();
+		m_map.emplace(std::move(strMsgType), std::forward<F>(f));
+	}
+
+private:
+	std::multimap<std::string, Any> m_map;
+	typedef std::multimap<std::string, Any>::iterator Iterater;
+};
---
+++ src/Server/test/test.cpp
@@ -0,0 +1,38 @@
+
+#include "stdafx.h"
+#include "./mesagebus/MessageBus.hpp"
+#include <iostream>
+
+struct A
+{
+    A()
+    {
+       std::cout << "A::A"<< std::endl;
+    }
+    ~A()
+    {
+        std::cout << "A::~A" << std::endl;
+    }
+};
+A& GetA()
+{   
+    A temp; 
+    std::cout << "XXXX" << std::endl;
+    return temp;
+}
+int _tmain(int argc, _TCHAR* argv[])
+{   
+    //A&& a = std::move(GetA());
+    //A& b = GetA();
+    //A c = GetA();
+    //std::cout << "===>" << std::endl;
+
+    MessageBus bus;
+    bus.Attach([](int &a) { std::cout << a++ << std::endl;}, "test");
+    int i = 1;
+    bus.SendReq<void, int&>(i, "test");
+    bus.SendReq<void, int&>(i, "test");
+
+	return 0;
+}
+
